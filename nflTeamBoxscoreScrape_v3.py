da37a21fbd81dc3ba7be6970b5b1f46150f3ab1d
Gordon Yun <38215999+6ord@users.noreply.github.com>
2019-09-21 21:57:14 -0400

    Add files via upload

--- nflTeamBoxscoreScrape_v3.py
+++ nflTeamBoxscoreScrape_v3.py
@@ -1,6 +1,10 @@
 import re, requests, bs4, csv, datetime
 import pandas as pd
 
+pd.set_option('display.max_columns',500)
+pd.set_option('display.max_rows',50)
+pd.set_option('display.width',1000)
+
 def boxscoreScrapeWk(wkNum):
     print(datetime.datetime.now())
     #Scrape CBS NFL schedule for week "wkNum", and generate boxscore links
@@ -8,16 +12,42 @@ def boxscoreScrapeWk(wkNum):
     #Ex, https://www.cbssports.com/nfl/gametracker/boxscore/NFL_20171210_IND@BUF
 
     cbs_url = 'https://www.cbssports.com'
-    schedule_url = cbs_url+'/nfl/schedule/2019/regular/'+str(wkNum)
+    #schedule_url = cbs_url+'/nfl/schedule/2019/regular/'+str(wkNum)
+    schedule_url = '/nfl/schedule/2019/regular/'
     
-    wkSchedSoup = bs4.BeautifulSoup(requests.get(schedule_url).text,'html.parser')
+    seasonDF = pd.DataFrame(columns=['Week','Date','Road','Home','url'])
+    # Ex, 1, YYYY-MM-DD, AWY, @HME, http://...AWY@HME/
+    
+    for i in range(1,18):
+        weekURL = cbs_url+schedule_url+str(i)
+        wkSoup = bs4.BeautifulSoup(requests.get(weekURL).text,'html.parser')
+        gameURLs = wkSoup.select('.CellGame a[href]')
+        numGm = len(gameURLs)
+        
+        #create DF of one week's games
+        thisWkDF = pd.DataFrame(columns=['Week','Date','Road','Home','url'])
+        
+        for j in range(numGm):
+            link = gameURLs[j].get('href')
+            p = link.find('NFL_')+4
+            thisWkDF.loc[j] = [i] + [str(link[p:p+4]+'-'+link[p+4:p+6]+'-'+link[p+6:p+8])]+[link[p+9:(link.find('@'))]] + [link[(link.find('@')):-1]]+[cbs_url+link] 
+
+        seasonDF = seasonDF.append(thisWkDF)        
+        #del(thisWkDF)
+        #append above DF to schedDF
+
+    gmURLs = seasonDF[seasonDF['Week']==wkNum]['url']
+    numGm = len(gmURLs)
+    #wkSchedSoup = bs4.BeautifulSoup(requests.get(cbs_url+schedule_url+str(wkNum)).text,'html.parser')
     #for 16 matches in i, generate dataframe, game boxscore URLs down one column
-    numGm = len(wkSchedSoup.select('.CellGame a[href]'))
-    gmURLs = pd.DataFrame(columns=['gm_url'])
+    #numGm = len(wkSchedSoup.select('.CellGame a[href]'))
     
-    for i in range(numGm):
-        gmURLs.loc[i] = cbs_url+str(wkSchedSoup.select('.CellGame a[href]')[i].get('href')).replace('recap','boxscore')
     
+    
+
+    #for i in range(numGm):
+    #    gmURLs.loc[i] = cbs_url+str(wkSchedSoup.select('.CellGame a[href]')[i].get('href')).replace('recap','boxscore')
+
     soupOutput = []
     tagDump = open('nflBoxscoreScrape2019(Tags).csv','a',newline='') #change to append
     tagCSVWriter = csv.writer(tagDump,delimiter=',',lineterminator='\n') #change to append
@@ -29,10 +59,17 @@ def boxscoreScrapeWk(wkNum):
 
     #######################################################
     ###  NEED SCHEDULE CSV BUT CAN DO FROM PANDA STRAIGHT TO CSV???
+
     
-    schdOutput = []
-    schdDump = open('2019_schedule_single.csv','a',newline='')
-    schdCSVWriter = csv.writer(realDump,delimiter=',',lineterminator='\n')
+
+    
+    #schdOutput = []
+    #schdDump = open('2019_schedule_single.csv','a',newline='')
+    #schdCSVWriter = csv.writer(realDump,delimiter=',',lineterminator='\n')
+
+    seasonDF.drop('url',axis=1).to_csv('2019_schedule_single.csv', sep = ',', encoding = 'utf-8', index = False)
+
+
 
     #######################################################
 
@@ -46,13 +83,14 @@ def boxscoreScrapeWk(wkNum):
 
     # Go through game url list and scrape each url
     for i in range(len(gmURLs)):
-        url=str(gmURLs.iloc[i,0])
+        url=str(gmURLs.loc[i])
         boxSoup = bs4.BeautifulSoup(requests.get(url).text,'html.parser')
-        
+
         # TESTING: Make sure each game has 36 stats(from bs4 Object length) 
         print('Gm '+str(i)+'  '+url+'   '+str(len(boxSoup.select(tagPick_lines))))
         print(datetime.datetime.now())
-        soupOutput.append([url[55:len(url)],boxSoup.select(tagPick_lines)[0].getText()])
+        p = url.find('NFL_')+4
+        soupOutput.append([url[p:-1],boxSoup.select(tagPick_lines)[0].getText()])
 
         for j in range(1,len(boxSoup.select(tagPick_lines))):
             soupOutput[i].append(boxSoup.select(tagPick_lines)[j].getText())
@@ -72,18 +110,21 @@ def boxscoreScrapeWk(wkNum):
     for h in range(1,len(soupOutput[0])):
         realOutput[nxt_rcrd].append(soupOutput[0][h].splitlines()[0])
     nxt_rcrd += 1    
-    
+
     for i in range(len(soupOutput)):
-        url=str(gmURLs.iloc[i,0])
-        realOutput.append([url[55:len(url)],
-                           url[55:63],
+        url=str(gmURLs.loc[0])
+        p = url.find('NFL_')+4    
+        realOutput.append([url[p:-1],
+                           url[p:p+8],
                            url[url.rfind('_')+1:url.find('@')]  #away team
                            ])
         for j in range(1,len(soupOutput[i])):
             realOutput[nxt_rcrd].append(soupOutput[i][j].splitlines()[1])
+
         nxt_rcrd += 1    
-        realOutput.append([url[55:len(url)],
-                           url[55:63],
+
+        realOutput.append([url[p:-1],
+                           url[p:p+8],
                            url[url.find('@')+1:len(url)-1]   #home team
                            ])
         for k in range(1,len(soupOutput[i])): #Do something different if j=34,35
@@ -101,17 +142,17 @@ def boxscoreScrapeWk(wkNum):
     realDump.close()
 
 
-    
-    
+
+
 
 # UNDER CONSTRUCTION #
 
 
 def snapcountScrape():
-    
+
     osc_url='https://www.fantasypros.com/nfl/reports/snap-counts/'
     dsc_url='https://www.fantasypros.com/nfl/reports/snap-counts/defense.php'
-    
+
     osc_soup=bs4.BeautifulSoup(requests.get(osc_url).text,'html.parser')
     dsc_soup=bs4.BeautifulSoup(requests.get(dsc_url).text,'html.parser')
 
@@ -126,7 +167,7 @@ def snapcountScrape():
 
     allSnapCounts=[]
     allSnapCounts.append(heading)
-    
+
     start=0
     end=22    
     for i in range(int(len(offTagList)/22)):
@@ -135,12 +176,12 @@ def snapcountScrape():
 
         for j in range(start,end):
             temp.append(offTagList[j].getText())
-        
+
         allSnapCounts.append(temp)
         del temp
         start+=22
         end+=22
-    
+
     start=0
     end=22    
     for i in range(int(len(defTagList)/22)):
@@ -149,7 +190,7 @@ def snapcountScrape():
 
         for j in range(start,end):
             temp.append(defTagList[j].getText())
-        
+
         allSnapCounts.append(temp)
         del temp
         start+=22
@@ -162,5 +203,3 @@ def snapcountScrape():
         for i in (range(len(allSnapCounts))):
             realCSVWriter.writerow(allSnapCounts[i])
         realDump.close()
-
-
